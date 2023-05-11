3a36bcbd25f9533e473d2b2ec62be928a2c011a8
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-05-11 10:43:46 +0700

    tes

--- exes_2_047_A/exes_2_047_A.cpp
+++ exes_2_047_A/exes_2_047_A.cpp
@@ -1,3 +1,33 @@
 #include <iostream>
 using namespace std;
 
+int binarisearch(int Isro[101], int n, int desiredelement) {
+	int lowerbound = 0;
+	int upperbound = n - 1;
+
+	while (lowerbound <= upperbound) {
+		int(lowerbound + upperbound) / 2;
+
+		if (Isro[101] = desiredelement) {
+			("found");
+			return 0;
+		}
+	}
+
+	if (desiredelement < Isro[101]) {
+		upperbound = 101 - 1;
+		return 0;
+	}
+	
+	if (desiredelement > Isro[101]) {
+		lowerbound = 101 + 1;
+	}
+
+	if (lowerbound <= upperbound) {
+			int(lowerbound + upperbound) / 2;
+		}
+
+	("Not Found"); {
+	return 0;
+	}
+}
